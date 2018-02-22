bd6a477e5003b4322a8a6604ed8b1fad8ceac8aa
jayaprakashprobeseven <jayaprakash.g@probeseven.com>
2018-02-22 14:52:42 +0530

    Google maps plugin installed and sample maps place marker created

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,66 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Platform } from "ionic-angular";
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  GoogleMapsAnimation
+} from "@ionic-native/google-maps";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  map: GoogleMap;
+  constructor(public navCtrl: NavController, public platform: Platform) {}
 
-  constructor(public navCtrl: NavController) {
-
+  ionViewDidLoad() {
+    this.platform.ready().then(readySource => {
+      console.log("Platform ready from", readySource);
+      this.loadMap();
+    });
   }
 
+  loadMap() {
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 11.0168,
+          lng: 76.9558
+        },
+        zoom: 15,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create("map_canvas", mapOptions);
+
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      console.log("Map is ready!");
+
+      // Now you can use all methods safely.
+      this.map
+        .addMarker({
+          title: "Ionic",
+          icon: "blue",
+          animation: "DROP",
+          position: {
+            lat: 11.0168,
+            lng: 76.9558
+          }
+        })
+        .then(marker => {
+          marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+            alert("clicked");
+          });
+        });
+    });
+  }
 }
